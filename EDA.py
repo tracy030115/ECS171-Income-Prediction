ce077f84b5d0257109f85e742fe0ec5ba4e7b9d6
Giorgi Tsiklauri <131500703+DabDaddy59@users.noreply.github.com>
2025-05-01 21:55:05 +0000

    EDA.py

--- EDA.py
+++ EDA.py
@@ -3,13 +3,26 @@ import pandas as pd
 
 dataFile = pd.read_csv('data.csv')
 
-i = 0;
 
 independents = ['Age','Education_Level','Occupation','Number_of_Dependents','Location','Work_Experience','Marital_Status','Employment_Status','Household_Size','Homeownership_Status','Type_of_Housing','Gender','Primary_Mode_of_Transportation']
 dependent = 'Income'
 
-plt.plot(df[independents[i]], df[dependent])
-plt.xlabel(independents[i])
-plt.ylabel(dependent)
-plt.title(dependent + ' vs ' + independent[i])
-plt.show()
+for i in range(len(independents)):
+    print(dependent + ' vs ' + independents[i] + '\n')
+    plt.scatter(dataFile[independents[i]], dataFile[dependent])
+    plt.yscale('log')
+    plt.xlabel(independents[i])
+    plt.ylabel(dependent)
+    plt.title(dependent + ' vs ' + independents[i])
+    plt.savefig(dependent + '_vs_' + independents[i] + "_scatter.png")
+    plt.cla()
+
+for i in range(len(independents)):
+    print(dependent + ' vs ' + independents[i] + '\n')
+    plt.bar(dataFile[independents[i]], dataFile[dependent])
+    plt.yscale('linear')
+    plt.xlabel(independents[i])
+    plt.ylabel(dependent)
+    plt.title(dependent + ' vs ' + independents[i])
+    plt.savefig(dependent + '_vs_' + independents[i] + "_bar.png")
+    plt.cla()
