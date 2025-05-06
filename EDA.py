7797405473cec2f1ad1154a9ffab5612dac8174b
Giorgi Tsiklauri <131500703+DabDaddy59@users.noreply.github.com>
2025-05-06 20:30:34 +0000

    EDA.py

--- EDA.py
+++ EDA.py
@@ -8,10 +8,37 @@ independents = ['Age','Education_Level','Occupation','Number_of_Dependents','Loc
 dependent = 'Income'
 
 for i in range(len(independents)):
-    print(dependent + ' vs ' + independents[i] + '\n')
+    print(dependent + ' vs ' + independents[i] + " Box\n")
+    grouped = dataFile.groupby(independents[i])[dependent]
+    independent = sorted(dataFile[independents[i]].unique())
+    income_by_independent = [grouped.get_group(j) for j in independent]
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.boxplot(income_by_independent, vert=True, patch_artist=True, tick_labels=independent, showfliers=False)
+    ax.set_xlabel(independents[i])
+    ax.set_ylabel(dependent)
+    plt.tight_layout()
+    plt.savefig(dependent + '_vs_' + independents[i] + "_box.png")
+    plt.cla()
+    plt.close()
+
+for i in range(len(independents)):
+    print(dependent + ' vs ' + independents[i] + " Box Outliers\n")
+    grouped = dataFile.groupby(independents[i])[dependent]
+    independent = sorted(dataFile[independents[i]].unique())
+    income_by_independent = [grouped.get_group(j) for j in independent]
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.boxplot(income_by_independent, vert=True, patch_artist=True, tick_labels=independent, showfliers=True)
+    ax.set_xlabel(independents[i])
+    ax.set_ylabel(dependent)
+    plt.tight_layout()
+    plt.savefig(dependent + '_vs_' + independents[i] + "_box_outliers.png")
+    plt.cla()
+    plt.close()
+
+for i in range(len(independents)):
+    print(dependent + ' vs ' + independents[i] + " Median Bar\n")
     medians = dataFile.groupby(independents[i])[dependent].median().reset_index()
-    print(medians)
-    exit()
     plt.bar(medians[independents[i]], medians[dependent])
     plt.yscale('linear')
     plt.xlabel(independents[i])
@@ -19,9 +46,10 @@ for i in range(len(independents)):
     plt.title(dependent + ' vs ' + independents[i])
     plt.savefig(dependent + '_vs_' + independents[i] + "_median_bar.png")
     plt.cla()
+    plt.close()
 
 for i in range(len(independents)):
-    print(dependent + ' vs ' + independents[i] + '\n')
+    print(dependent + ' vs ' + independents[i] + " Median Plot\n")
     medians = dataFile.groupby(independents[i])[dependent].median().reset_index()
     plt.plot(medians[independents[i]], medians[dependent])
     plt.yscale('linear')
@@ -30,9 +58,10 @@ for i in range(len(independents)):
     plt.title(dependent + ' vs ' + independents[i])
     plt.savefig(dependent + '_vs_' + independents[i] + "_median_plot.png")
     plt.cla()
+    plt.close()
 
 for i in range(len(independents)):
-    print(dependent + ' vs ' + independents[i] + '\n')
+    print(dependent + ' vs ' + independents[i] + " Scatter\n")
     plt.scatter(dataFile[independents[i]], dataFile[dependent])
     plt.yscale('linear')
     plt.xlabel(independents[i])
@@ -40,9 +69,10 @@ for i in range(len(independents)):
     plt.title(dependent + ' vs ' + independents[i])
     plt.savefig(dependent + '_vs_' + independents[i] + "_scatter.png")
     plt.cla()
+    plt.close()
 
 for i in range(len(independents)):
-    print(dependent + ' vs ' + independents[i] + '\n')
+    print(dependent + ' vs ' + independents[i] + " Bar\n")
     plt.bar(dataFile[independents[i]], dataFile[dependent])
     plt.yscale('linear')
     plt.xlabel(independents[i])
@@ -50,3 +80,4 @@ for i in range(len(independents)):
     plt.title(dependent + ' vs ' + independents[i])
     plt.savefig(dependent + '_vs_' + independents[i] + "_bar.png")
     plt.cla()
+    plt.close()
